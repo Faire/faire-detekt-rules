ce488332f3951cfbf143a595ef3d113c6aa2d000
Robert Ma <robert.ma@faire.com>
2024-05-06 16:13:48 -0700

    no wildcard import

--- src/main/kotlin/com/faire/detekt/rules/NoFunctionReferenceToJavaClass.kt
+++ src/main/kotlin/com/faire/detekt/rules/NoFunctionReferenceToJavaClass.kt
@@ -1,6 +1,12 @@
 package com.faire.detekt.rules
 
-import io.gitlab.arturbosch.detekt.api.*
+import io.gitlab.arturbosch.detekt.api.CodeSmell
+import io.gitlab.arturbosch.detekt.api.Config
+import io.gitlab.arturbosch.detekt.api.Debt
+import io.gitlab.arturbosch.detekt.api.Entity
+import io.gitlab.arturbosch.detekt.api.Issue
+import io.gitlab.arturbosch.detekt.api.Rule
+import io.gitlab.arturbosch.detekt.api.Severity
 import org.jetbrains.kotlin.psi.KtCallableReferenceExpression
 
 /**
