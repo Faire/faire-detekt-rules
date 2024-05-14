5654b213f218553934ae9291359b0c89e12ffd09
AndrewZeitler <azeitler@live.ca>
2024-05-14 10:30:05 -0400

    Adding maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "1.9.24"
-    `maven-publish`
+    alias(libs.plugins.maven.publishing)
 
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
@@ -54,10 +56,44 @@ kotlin {
     jvmToolchain(8)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.DEFAULT, true)
+    signAllPublications()
+
+    pom {
+        name = "faire-detekt-rules"
+        description = "Detekt rules for Faire"
+        inceptionYear = "2023"
+        url = "https://github.com/Faire/faire-detekt-rules"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                distribution = "repo"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        developers {
+            developer {
+                id = "Faire"
+                name = "Faire Developers"
+            }
+        }
+
+        organization {
+            name = "Faire"
+            url = "https://www.faire.com"
+        }
+
+        issueManagement {
+            system = "GitHub"
+            url = "https://github.com/Faire/faire-detekt-rules/issues"
+        }
+
+        scm {
+            connection = "scm:git:git://github.com/Faire/faire-detekt-rules.git"
+            url = "https://github.com/Faire/faire-detekt-rules"
         }
     }
 }
