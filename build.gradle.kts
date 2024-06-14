fd88b6814b65e75051fcb5dc295321f8c1c258ec
Kevin Brightwell <kevin.brightwell@faire.com>
2024-06-14 12:55:58 -0400

    Release: Prepare for v0.2.5, update Release pattern. (#43)
    Bump release version, update release patterns to be closer to convention.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,99 +1,115 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-    kotlin("jvm") version "1.9.24"
-    alias(libs.plugins.maven.publishing)
+  alias(libs.plugins.kotlin)
+  alias(libs.plugins.maven.publishing)
 
-    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+  alias(libs.plugins.detekt)
 }
 
-group = "com.faire.detektrules"
-version = "0.1.0"
+group = "com.faire"
+version = "0.2.5"
 
-val detektVersion = "1.23.6"
+if (!providers.environmentVariable("RELEASE").isPresent) {
+  val gitSha = providers.environmentVariable("GITHUB_SHA")
+    .orElse(
+      provider {
+        // nest the provider, we don't want to invalidate the config cache for this
+        providers.exec { commandLine("git", "rev-parse", "--short", "HEAD") }
+          .standardOutput
+          .asText
+          .get()
+      }
+    )
+    .get()
+
+  version = "$version-$gitSha-SNAPSHOT"
+}
+
+val detektVersion = libs.versions.detekt.get()
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("compiler-embeddable"))
-    runtimeOnly(kotlin("reflect"))
+  implementation(kotlin("stdlib"))
+  implementation(kotlin("compiler-embeddable"))
+  runtimeOnly(kotlin("reflect"))
 
-    implementation(libs.detekt.api)
+  implementation(libs.detekt.api)
 
-    runtimeOnly(libs.detekt.formatting)
+  runtimeOnly(libs.detekt.formatting)
 
-    testImplementation(libs.assertj)
-    testImplementation(libs.detekt.test)
-    testImplementation(libs.guava)
-    testImplementation(libs.junit.jupiter.api)
+  testImplementation(libs.assertj)
+  testImplementation(libs.detekt.test)
+  testImplementation(libs.guava)
+  testImplementation(libs.junit.jupiter.api)
 
-    testRuntimeOnly(libs.junit.jupiter.engine)
+  testRuntimeOnly(libs.junit.jupiter.engine)
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:$detektVersion")
-    detektPlugins("com.braisgabin.detekt:kotlin-compiler-wrapper:0.0.4")
-    detektPlugins(rootProject)
+  detektPlugins(libs.detekt.formatting)
+  detektPlugins(libs.detekt.ruleauthors)
+  detektPlugins(libs.detekt.compiler.wrapper)
+  detektPlugins(rootProject)
 }
 
 detekt {
-    parallel = true
-    autoCorrect = true
+  parallel = true
+  autoCorrect = true
 
-    buildUponDefaultConfig = true
-    config.from(rootProject.file("detekt.yaml"))
+  buildUponDefaultConfig = true
+  config.from(rootProject.file("detekt.yaml"))
 
-    allRules = false // activate all available (even unstable) rules.
+  allRules = false // activate all available (even unstable) rules.
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(8)
+  jvmToolchain(8)
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.DEFAULT, true)
-    signAllPublications()
+  publishToMavenCentral(SonatypeHost.DEFAULT, true)
+  signAllPublications()
 
-    pom {
-        name = "faire-detekt-rules"
-        description = "Detekt rules for Faire"
-        inceptionYear = "2023"
-        url = "https://github.com/Faire/faire-detekt-rules"
+  pom {
+    name = "faire-detekt-rules"
+    description = "Detekt rules for Faire"
+    inceptionYear = "2023"
+    url = "https://github.com/Faire/faire-detekt-rules"
 
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                distribution = "repo"
-                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-
-        developers {
-            developer {
-                id = "Faire"
-                name = "Faire Developers"
-            }
-        }
-
-        organization {
-            name = "Faire"
-            url = "https://www.faire.com"
-        }
-
-        issueManagement {
-            system = "GitHub"
-            url = "https://github.com/Faire/faire-detekt-rules/issues"
-        }
-
-        scm {
-            connection = "scm:git:git://github.com/Faire/faire-detekt-rules.git"
-            url = "https://github.com/Faire/faire-detekt-rules"
-        }
+    licenses {
+      license {
+        name = "The Apache License, Version 2.0"
+        distribution = "repo"
+        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+      }
     }
+
+    developers {
+      developer {
+        id = "Faire"
+        name = "Faire Developers"
+      }
+    }
+
+    organization {
+      name = "Faire"
+      url = "https://www.faire.com"
+    }
+
+    issueManagement {
+      system = "GitHub"
+      url = "https://github.com/Faire/faire-detekt-rules/issues"
+    }
+
+    scm {
+      connection = "scm:git:git://github.com/Faire/faire-detekt-rules.git"
+      url = "https://github.com/Faire/faire-detekt-rules"
+    }
+  }
 }
