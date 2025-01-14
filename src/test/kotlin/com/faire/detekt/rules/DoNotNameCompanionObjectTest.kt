92cdc6cfccf71438034f9b7b709825a54ab67d06
Matt Gulbronson <59572380+mgulbronson@users.noreply.github.com>
2025-01-14 10:50:41 -0500

    [Proposal] Ban named companion objects (#79)
    See KDoc comment on the rule for rationale.

---
+++ src/test/kotlin/com/faire/detekt/rules/DoNotNameCompanionObjectTest.kt
@@ -0,0 +1,51 @@
+package com.faire.detekt.rules
+
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class DoNotNameCompanionObjectTest {
+  private lateinit var rule: DoNotNameCompanionObject
+
+  @BeforeEach
+  fun setup() {
+    rule = DoNotNameCompanionObject()
+  }
+
+  @Test
+  fun `named companion object is flagged`() {
+    val findings = rule.lint(
+      """
+          class Foo {
+            companion object Bar
+          }
+        """.trimIndent(),
+    )
+    assertThat(findings).hasSize(1)
+  }
+
+  @Test
+  fun `unnamed companion object is not flagged`() {
+    val findings = rule.lint(
+      """
+          class Foo {
+            companion object
+          }
+        """.trimIndent(),
+    )
+    assertThat(findings).isEmpty()
+  }
+
+  @Test
+  fun `named non-companion object is not flagged`() {
+    val findings = rule.lint(
+      """
+          class Foo {
+            object Bar
+          }
+        """.trimIndent(),
+    )
+    assertThat(findings).isEmpty()
+  }
+}
