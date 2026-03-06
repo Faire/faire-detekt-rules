1837ad8a576f8c6c0d81c4b56d3591ee21696b7c
Travis Miehm <travis.miehm@faire.com>
2026-03-06 09:31:08 -0500

    Use simpler RuleSet constructor

--- src/main/kotlin/com/faire/detekt/FaireRulesProvider.kt
+++ src/main/kotlin/com/faire/detekt/FaireRulesProvider.kt
@@ -33,51 +33,49 @@ import com.faire.detekt.rules.UseMapNotNullInsteadOfFilterNotNull
 import com.faire.detekt.rules.UseNoneMatchInsteadOfFirstOrNullIsNull
 import com.faire.detekt.rules.UseOfCollectionInsteadOfEmptyCollection
 import com.faire.detekt.rules.UseSetInsteadOfListToSet
-import dev.detekt.api.RuleName
 import dev.detekt.api.RuleSet
 import dev.detekt.api.RuleSetId
 import dev.detekt.api.RuleSetProvider
-import kotlin.to
 
 internal class FaireRulesProvider : RuleSetProvider {
   override val ruleSetId = RuleSetId("FaireRuleSet")
 
   override fun instance(): RuleSet = RuleSet(
       ruleSetId,
-      mapOf(
-          RuleName(AlwaysUseIsTrueOrIsFalse::class.simpleName!!) to { config -> AlwaysUseIsTrueOrIsFalse(config) },
-          RuleName(DoNotAccessVisibleForTesting::class.simpleName!!) to { config -> DoNotAccessVisibleForTesting(config) },
-          RuleName(DoNotAssertIsEqualOnTheResultOfSingle::class.simpleName!!) to { config -> DoNotAssertIsEqualOnTheResultOfSingle(config) },
-          RuleName(DoNotNameCompanionObject::class.simpleName!!) to { config -> DoNotNameCompanionObject(config) },
-          RuleName(DoNotSplitByRegex::class.simpleName!!) to { config -> DoNotSplitByRegex(config) },
-          RuleName(DoNotUseDirectReceiverReferenceInsideWith::class.simpleName!!) to { config -> DoNotUseDirectReceiverReferenceInsideWith(config) },
-          RuleName(DoNotUsePropertyAccessInAssert::class.simpleName!!) to { config -> DoNotUsePropertyAccessInAssert(config) },
-          RuleName(DoNotUseHasSizeForEmptyListInAssert::class.simpleName!!) to { config -> DoNotUseHasSizeForEmptyListInAssert(config) },
-          RuleName(DoNotUseIsEqualToWhenArgumentIsOne::class.simpleName!!) to { config -> DoNotUseIsEqualToWhenArgumentIsOne(config) },
-          RuleName(DoNotUseIsEqualToWhenArgumentIsZero::class.simpleName!!) to { config -> DoNotUseIsEqualToWhenArgumentIsZero(config) },
-          RuleName(DoNotUseIsOneAssertions::class.simpleName!!) to { config -> DoNotUseIsOneAssertions(config) },
-          RuleName(DoNotUseIsZeroAssertions::class.simpleName!!) to { config -> DoNotUseIsZeroAssertions(config) },
-          RuleName(DoNotUseSingleOnFilter::class.simpleName!!) to { config -> DoNotUseSingleOnFilter(config) },
-          RuleName(DoNotUseSizePropertyInAssert::class.simpleName!!) to { config -> DoNotUseSizePropertyInAssert(config) },
-          RuleName(FilterNotNullOverMapNotNullForFiltering::class.simpleName!!) to { config -> FilterNotNullOverMapNotNullForFiltering(config) },
-          RuleName(GetOrDefaultShouldBeReplacedWithGetOrElse::class.simpleName!!) to { config -> GetOrDefaultShouldBeReplacedWithGetOrElse(config) },
-          RuleName(NoDuplicateKeysInMapOf::class.simpleName!!) to { config -> NoDuplicateKeysInMapOf(config) },
-          RuleName(NoEmptyLinesInConstructorParameters::class.simpleName!!) to { config -> NoEmptyLinesInConstructorParameters(config) },
-          RuleName(NoExtensionFunctionOnNullableReceiver::class.simpleName!!) to { config -> NoExtensionFunctionOnNullableReceiver(config) },
-          RuleName(NoFunctionReferenceToJavaClass::class.simpleName!!) to { config -> NoFunctionReferenceToJavaClass(config) },
-          RuleName(NoNonPrivateGlobalVariables::class.simpleName!!) to { config -> NoNonPrivateGlobalVariables(config) },
-          RuleName(NoNullableLambdaWithDefaultNull::class.simpleName!!) to { config -> NoNullableLambdaWithDefaultNull(config) },
-          RuleName(NoPairWithAmbiguousTypes::class.simpleName!!) to { config -> NoPairWithAmbiguousTypes(config) },
-          RuleName(PreferIgnoreCase::class.simpleName!!) to { config -> PreferIgnoreCase(config) },
-          RuleName(PreventBannedImports::class.simpleName!!) to { config -> PreventBannedImports(config) },
-          RuleName(ReturnValueOfLetMustBeUsed::class.simpleName!!) to { config -> ReturnValueOfLetMustBeUsed(config) },
-          RuleName(UseEntriesInsteadOfValuesOnEnum::class.simpleName!!) to { config -> UseEntriesInsteadOfValuesOnEnum(config) },
-          RuleName(UseFirstNotNullOf::class.simpleName!!) to { config -> UseFirstNotNullOf(config) },
-          RuleName(UseFirstOrNullInsteadOfFind::class.simpleName!!) to { config -> UseFirstOrNullInsteadOfFind(config) },
-          RuleName(UseMapNotNullInsteadOfFilterNotNull::class.simpleName!!) to { config -> UseMapNotNullInsteadOfFilterNotNull(config) },
-          RuleName(UseNoneMatchInsteadOfFirstOrNullIsNull::class.simpleName!!) to { config -> UseNoneMatchInsteadOfFirstOrNullIsNull(config) },
-          RuleName(UseOfCollectionInsteadOfEmptyCollection::class.simpleName!!) to { config -> UseOfCollectionInsteadOfEmptyCollection(config) },
-          RuleName(UseSetInsteadOfListToSet::class.simpleName!!) to { config -> UseSetInsteadOfListToSet(config) },
+      listOf(
+          { AlwaysUseIsTrueOrIsFalse(it) },
+          { DoNotAccessVisibleForTesting(it) },
+          { DoNotAssertIsEqualOnTheResultOfSingle(it) },
+          { DoNotNameCompanionObject(it) },
+          { DoNotSplitByRegex(it) },
+          { DoNotUseDirectReceiverReferenceInsideWith(it) },
+          { DoNotUsePropertyAccessInAssert(it) },
+          { DoNotUseHasSizeForEmptyListInAssert(it) },
+          { DoNotUseIsEqualToWhenArgumentIsOne(it) },
+          { DoNotUseIsEqualToWhenArgumentIsZero(it) },
+          { DoNotUseIsOneAssertions(it) },
+          { DoNotUseIsZeroAssertions(it) },
+          { DoNotUseSingleOnFilter(it) },
+          { DoNotUseSizePropertyInAssert(it) },
+          { FilterNotNullOverMapNotNullForFiltering(it) },
+          { GetOrDefaultShouldBeReplacedWithGetOrElse(it) },
+          { NoDuplicateKeysInMapOf(it) },
+          { NoEmptyLinesInConstructorParameters(it) },
+          { NoExtensionFunctionOnNullableReceiver(it) },
+          { NoFunctionReferenceToJavaClass(it) },
+          { NoNonPrivateGlobalVariables(it) },
+          { NoNullableLambdaWithDefaultNull(it) },
+          { NoPairWithAmbiguousTypes(it) },
+          { PreferIgnoreCase(it) },
+          { PreventBannedImports(it) },
+          { ReturnValueOfLetMustBeUsed(it) },
+          { UseEntriesInsteadOfValuesOnEnum(it) },
+          { UseFirstNotNullOf(it) },
+          { UseFirstOrNullInsteadOfFind(it) },
+          { UseMapNotNullInsteadOfFilterNotNull(it) },
+          { UseNoneMatchInsteadOfFirstOrNullIsNull(it) },
+          { UseOfCollectionInsteadOfEmptyCollection(it) },
+          { UseSetInsteadOfListToSet(it) },
       ),
   )
 }
